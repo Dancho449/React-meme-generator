98876a0a912c5cc75e95901cf724d1a19da8de95
Dancho449 <125226463+Dancho449@users.noreply.github.com>
2023-06-22 14:53:40 -0700

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,11 @@ export default function Header() {
     return(
         <header className="top-header">
             <div className="logo">
-                <img src="../../public/images/Pepe-stare.png" className="meme-logo"/>
+                <img src="../images/Pepe-stare.png" className="meme-logo"/>
                 <h2>Dancho's Meme-Generator</h2>
             </div>
             <h4>React Course - Project 3</h4>
         </header>
     )
 }
+/*../../public/images/Pepe-stare.png*/
