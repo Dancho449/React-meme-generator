17d3a09091ba618b9a20adada2b8ecd265cb22f0
Dancho449 <125226463+Dancho449@users.noreply.github.com>
2023-07-03 15:25:54 -0700

    update again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react"
+import Pepe from "../images/Pepe-stare.png"
 
 export default function Header() {
     return(
         <header className="top-header">
             <div className="logo">
-                <img src="/images/Pepe-stare.png" className="meme-logo"/>
+                <img src={Pepe} className="meme-logo"/>
                 <h2>Dancho's Meme-Generator</h2>
             </div>
             <h4>React Course - Project 3</h4>
         </header>
     )
 }
+/* "/images/Pepe-stare.png" */
